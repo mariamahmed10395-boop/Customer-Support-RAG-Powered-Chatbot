1e5d4732c37ca0289f4cbd13d2f828128fa5b11f
Lojain-Maged <lojimaged@icloud.com>
2026-05-03 00:49:30 +0300

    Update build_chroma.py

--- Customer-Support-RAG-Powered-Chatbot/vector_store/build_chroma.py
+++ Customer-Support-RAG-Powered-Chatbot/vector_store/build_chroma.py
@@ -1,1 +1,22 @@
+from langchain.schema import Document
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
 
+docs = [
+    Document(page_content="سياسة الإرجاع خلال 24 ساعة"),
+    Document(page_content="يمكن استبدال المنتج خلال 7 أيام")
+]
+
+embedding = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+db = Chroma.from_documents(
+    docs,
+    embedding,
+    persist_directory="chroma_db_store"
+)
+
+db.persist()
+
+print("done")
