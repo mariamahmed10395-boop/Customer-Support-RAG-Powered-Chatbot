c018e27a72c9823e7996946034b4ec48abfb20a7
Sara mohammed Zaki <Saramohammedzaki@gmail.com>
2026-07-01 00:08:30 +0300

    update project file

--- Customer-Support-RAG-Powered-Chatbot/retriever.py
+++ Customer-Support-RAG-Powered-Chatbot/retriever.py
@@ -9,7 +9,6 @@ cache_dir = "Customer-Support-RAG-Powered-Chatbot/embeddings_cache"
 if not os.path.exists(cache_dir):
     cache_dir = "embeddings_cache"
 
-
 index = faiss.read_index(os.path.join(cache_dir, "faiss.index"))
 
 with open(os.path.join(cache_dir, "meta.json"), "r", encoding="utf-8") as f:
@@ -20,11 +19,10 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def get_relevant_context(query: str, k: int = 3):
     """
-    دالة تأخذ سؤال العميل وترجع أقرب k سياقات (Context) من قاعدة البيانات
-    مخصصة للاستخدام مباشرة داخل LangGraph أو FastAPI
+    بتاخد سؤال العميل وترجع أقرب إجابات وسياق مناسب من الداتا المنظفة
     """
     try:
-       
+    
         query_vector = model.encode([query], convert_to_numpy=True)
         
        
@@ -34,10 +32,9 @@ def get_relevant_context(query: str, k: int = 3):
         for idx in indices[0]:
             if idx < len(meta_data):
                 item = meta_data[idx]
-                
-                context_list.append(item.get('context', f"السؤال: {item['question']}\nالإجابة: {item['answer']}"))
+                context_list.append(f"السؤال: {item['question']}\nالإجابة: {item['answer']}")
         
-        
+      
         return "\n\n".join(context_list)
     except Exception as e:
         print(f"Error in retrieval: {e}")
@@ -45,6 +42,5 @@ def get_relevant_context(query: str, k: int = 3):
 
 
 if __name__ == "__main__":
-    test_query = "طريقة استرجاع الطلب أو المنتج"
-    print("--- جاري تجربة الـ Retriever المطور ---")
-    print(get_relevant_context(test_query, k=2))
+    test_res = get_relevant_context("تأخير الشحن")
+    print(test_res)
